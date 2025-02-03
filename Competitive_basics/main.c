5a96ef38302cd148c247a35d973704f5a816a324
redowan07 <redowanbhuiyan1234098@gmail.com>
2025-02-03 12:20:19 +0600

    Initial Commit

---
+++ Competitive_basics/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Competitive_basics
+//
+//  Created by MacBook Air M1 on 3/2/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
